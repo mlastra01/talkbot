f4f8354f0018efbabfaf0ec7c09696f8a2afea8e
mlastra01 <mlastra@est.frba.utn.edu.ar>
2021-03-22 02:39:25 -0300

    first commit

---
+++ talkbot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace talkbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
